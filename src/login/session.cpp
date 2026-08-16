d0140abc557ab3876024583ed1c02f1aa76bc657
gagan <gagandeep.narula@students.iiit.ac.in>
2026-08-16 15:15:02 +0530

    Add session expiry check logic

--- src/login/session.cpp
+++ src/login/session.cpp
@@ -2,3 +2,7 @@
 std::string generateSessionToken(const std::string &username) {
     return "session_" + username + "_token";
 }
+
+bool isSessionExpired(long issuedAt, long now, long ttlSeconds) {
+    return (now - issuedAt) > ttlSeconds;
+}
